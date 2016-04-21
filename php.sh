7c1815112297e8d349b94d846e681f9cdf475d12
Frederico <Martini>
2016-04-20 23:39:42 -0300

    updated installation php

--- php.sh
+++ php.sh
@@ -2,7 +2,8 @@
 PHP_DIR='/etc/php5/apache2'
 
 apt-get update
-apt-get install php5 -y 
+apt-get install php5 php5-curl php5-gd php5-mcrypt php5-intl php-apc php5-sqlite php5-mysql php5-pgsql php-pear php5-dev -y
+
 INSTALL_SUCCESS=$?
 
 #verifica se foi instalado com sucesso
