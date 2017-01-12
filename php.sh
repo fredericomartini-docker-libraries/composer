690a0e00e85a96740da410ea4f986677ad6e8a9f
Frederico Martini <fredmalmeida@gmail.com>
2017-01-12 18:22:33 -0200

    adicionado php5-gmp na instalação para utilização com composer

--- php.sh
+++ php.sh
@@ -2,7 +2,7 @@
 PHP_DIR='/etc/php5/apache2'
 
 apt-get update
-apt-get install php5 php5-curl php5-gd php5-mcrypt php5-intl php-apc php5-sqlite php5-mysql php5-pgsql php-pear php5-dev -y
+apt-get install php5 php5-curl php5-gd php5-mcrypt php5-intl php-apc php5-sqlite php5-mysql php5-pgsql php-pear php5-dev php5-gmp -y
 
 INSTALL_SUCCESS=$?
 
